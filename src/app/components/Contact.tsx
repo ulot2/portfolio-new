6b76c678a5d6602313fe4bbaeaa29f2ca1be27a3
Tolu <tolu.nuell@gmail.com>
2025-09-03 14:34:10 +0100

    fixed a bug

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -85,7 +85,7 @@ export const Contact = () => {
             </div>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, x: 50 }}
+            initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.4, ease: "easeOut" }}
             viewport={{ once: true }}
