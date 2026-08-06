e0c571517fc9fc2d60694053fbf1ef1777dbb9ad
Tolu <tolu.nuell@gmail.com>
2026-08-06 11:13:57 +0100

    commented out the portfolio AI chatbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import { Contact } from "./components/Contact";
-import { PortfolioChat } from "./components/PortfolioChat";
+// import { PortfolioChat } from "./components/PortfolioChat";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Contact />
-      <PortfolioChat />
+      {/* <PortfolioChat /> */}
     </main>
   );
 }
